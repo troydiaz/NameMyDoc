e18a1ec35c447f0213b951ccfa2d10883e9260c6
Troy Diaz <troyk1718@gmail.com>
2025-04-25 15:40:21 -0700

    Adding settings bar for prefix, suffix, and delimiter. No functionality yet.

--- name-my-doc/src/components/upload/Upload.tsx
+++ name-my-doc/src/components/upload/Upload.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dropbox from '../dropbox/Dropbox';
+import SettingsBar from '../settingsbar/SettingsBar';
 
 const Upload: React.FC = () => {
+    const [prefix, setPrefix] = useState("");
+    const [suffix, setSuffix] = useState("");
+    const [delimiter, setDelimiter] = useState("");
+
     return (
         <div className="py-15">
-            <Dropbox />
+            <div className="flex flex-col justify-between gap-4 h-full">
+                <Dropbox />
+                <div className="flex justify-center">
+                    <SettingsBar 
+                    prefix={prefix}
+                    suffix={suffix}
+                    delimiter={delimiter}
+                    setDelimiter={setDelimiter}
+                    setPrefix={setPrefix}
+                    setSuffix={setSuffix}/>
+                </div>
+            </div>
         </div>
     )
 }
